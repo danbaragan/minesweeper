1f8928390b9a5345958a421dbf7c89e1211abcfd
Dan Bărăgan <daniel.mihai.baragan@gmail.com>
2021-03-13 15:55:17 +0200

    one page

--- js/game.js
+++ js/game.js
@@ -1,210 +1,223 @@
-const game = {
-    init: function () {
-        this.drawBoard();
-        this.availableFlagsCount = 0;
-        this.timeStart = 0;
-        document.getElementById('elapsed-time-counter').value = 0
-        this.updateFlags(this.mineCount);
-        // weirdly enough - this only works on Firefox.
-        // Although MDN says that Chrome has support since v66
-        // I enven started a discussion on https://bugs.chromium.org/p/chromium/issues/detail?id=1146467
-        this.unregisterController = new AbortController();
-        this.initLeftClick();
-        this.initRightClick();
+window.onload = () => {
+    const game = {
+        init: function () {
+            this.drawBoard();
+            this.availableFlagsCount = 0;
+            this.timeStart = 0;
+            document.getElementById('elapsed-time-counter').innerText = 0
+            this.updateFlags(this.mineCount);
+            // weirdly enough - this only works on Firefox.
+            // Although MDN says that Chrome has support since v66
+            // I enven started a discussion on https://bugs.chromium.org/p/chromium/issues/detail?id=1146467
+            this.unregisterController = new AbortController();
+            this.initLeftClick();
+            this.initRightClick();
 
-        this.mines2colorClass = [null, 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight']
-    },
+            this.mines2colorClass = [null, 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight']
+        },
 
-    drawBoard: function () {
-        const queryString = window.location.search;
-        const urlParams = new URLSearchParams(queryString);
-        this.rows = parseInt(urlParams.get('rows'));
-        this.cols = parseInt(urlParams.get('cols'));
-        this.mineCount = parseInt(urlParams.get('mines'));
-        const minePlaces = this.getRandomMineIndexes(this.mineCount, this.cols, this.rows);
+        drawBoard: function () {
+            const queryString = window.location.hash.slice(1);
+            const urlParams = new URLSearchParams(queryString);
+            this.rows = parseInt(urlParams.get('size'));
+            this.cols = this.rows
+            this.mineCount = parseInt(urlParams.get('mines'));
+            const minePlaces = this.getRandomMineIndexes(this.mineCount, this.cols, this.rows);
 
-        let gameField = document.querySelector(".game-field");
-        this.setGameFieldSize(gameField, this.rows, this.cols);
-        let cellIndex = 0
-        for (let row = 0; row < this.rows; row++) {
-            const rowElement = this.addRow(gameField);
-            for (let col = 0; col < this.cols; col++) {
-                this.addCell(rowElement, row, col, minePlaces.has(cellIndex));
-                cellIndex++;
-            }
-        }
-    },
-
-    getRandomMineIndexes: function (mineCount, cols, rows) {
-        const cellCount = cols * rows;
-        let mines = new Set();
-        do {
-            mines.add(Math.floor(Math.random() * cellCount));
-        } while (mines.size < mineCount && mines.size < cellCount);
-        return mines;
-    },
-
-    setGameFieldSize: function (gameField, rows, cols) {
-        gameField.style.width = (gameField.dataset.cellWidth * rows) + 'px';
-        gameField.style.height = (gameField.dataset.cellHeight * cols) + 'px';
-    },
-
-    addRow: function (gameField) {
-        gameField.insertAdjacentHTML(
-            'beforeend',
-            '<div class="row"></div>'
-        );
-        return gameField.lastElementChild;
-    },
-
-    addCell: function (rowElement, row, col, isMine) {
-        rowElement.insertAdjacentHTML(
-            'beforeend',
-            `<div class="field${isMine ? ' mine' : ''}"
-                        data-row="${row}" data-col="${col}"></div>`);
-    },
-
-    updateFlags: function(incValue) {
-        let newCount = this.availableFlagsCount + incValue
-        if (newCount >= 0 && newCount <= this.mineCount) {
-            this.availableFlagsCount = newCount
-            const elem = document.getElementById('flags-left-counter')
-            elem.value = this.availableFlagsCount
-            return true
-        }
-        return false
-    },
-
-    isWon: function() {
-        const openedFields = document.querySelectorAll('div.field.open')
-        if (this.rows * this.cols - openedFields.length === this.mineCount) {
-            return true
-        }
-    },
-
-    updateTime: function() {
-        const now = Math.floor(Date.now() / 1000)
-        const seconds = now - this.timeStart
-        const elem = document.getElementById('elapsed-time-counter')
-        elem.value = seconds
-        return seconds
-    },
-
-    // reference solution for "Create mine flagging feature" user story
-    initRightClick: function () {
-        // avoid the usage of global game variable from below
-        const rightClickHandler = (function(game) {
-            return function(e) {
-                if (game.isWon()) {
-                    game.unregisterController.abort()
-                    return
-                }
-                if (!e.target.classList.contains('field')) return;
-                e.preventDefault();
-                if (!game.timeStart) game.timeStart = Math.floor(Date.now() / 1000);
-
-                // if we already have flag - then we clear it and increase the available flags
-                const inc = e.target.classList.contains('flagged') ? 1 : -1
-                if (game.updateFlags(inc)) {
-                    e.target.classList.toggle('flagged');
+            let gameField = document.getElementById("game-field");
+            this.setGameFieldSize(gameField, this.rows, this.cols);
+            let cellIndex = 0
+            for (let row = 0; row < this.rows; row++) {
+                const rowElement = this.addRow(gameField);
+                for (let col = 0; col < this.cols; col++) {
+                    this.addCell(rowElement, row, col, minePlaces.has(cellIndex));
+                    cellIndex++;
                 }
             }
-        })(this);
+        },
 
-        // let's put this high up on the whole field and let it bubble up from the specific game tile
-        const gameField = document.getElementById("game-field");
-        gameField.addEventListener('contextmenu', rightClickHandler)
-    },
+        getRandomMineIndexes: function (mineCount, cols, rows) {
+            const cellCount = cols * rows;
+            let mines = new Set();
+            do {
+                mines.add(Math.floor(Math.random() * cellCount));
+            } while (mines.size < mineCount && mines.size < cellCount);
+            return mines;
+        },
 
-    initLeftClick: function () {
-        const leftClickHandler = (function(game) {
-            return function (e) {
-                if (!e.target.classList.contains('field')) return;
-                e.preventDefault()
-                if (!game.timeStart) game.timeStart = Math.floor(Date.now() / 1000);
+        setGameFieldSize: function (gameField, rows, cols) {
+            gameField.innerHTML = ''
+            gameField.style.width = (gameField.dataset.cellWidth * rows) + 'px';
+            gameField.style.height = (gameField.dataset.cellHeight * cols) + 'px';
+        },
 
-                const field = e.target
-                const already = field.classList.contains('open') || field.classList.contains('flagged')
-                if (already) return
+        addRow: function (gameField) {
+            gameField.insertAdjacentHTML(
+                'beforeend',
+                '<div class="row"></div>'
+            );
+            return gameField.lastElementChild;
+        },
 
-                field.classList.add('open')
+        addCell: function (rowElement, row, col, isMine) {
+            rowElement.insertAdjacentHTML(
+                'beforeend',
+                `<div class="field${isMine ? ' mine' : ''}"
+                            data-row="${row}" data-col="${col}"></div>`);
+        },
 
-                if (field.classList.contains('mine')) {
-                    const seconds = game.updateTime()
-                    window.alert(`💣  \n(in ${seconds} sec)`)
-
-                    const mineFields = document.querySelectorAll('div.field.mine')
-                    mineFields.forEach((f) => f.classList.add('open'))
-
-                    game.unregisterController.abort()
-                    return
-                    // history.back()
-                }
-
-                game.updateGameField(field)
-
-                if (game.isWon()) {
-                    const seconds = game.updateTime()
-                    window.alert(`🎉 \n(in ${seconds} sec)`)
-
-                    const mineFields = document.querySelectorAll('div.field.mine')
-                    mineFields.forEach((f) => f.classList.add('flagged'))
-                    game.updateFlags(-1)  // should be only one mine left to flag
-
-                    game.unregisterController.abort()
-                    return
-                }
+        updateFlags: function(incValue) {
+            let newCount = this.availableFlagsCount + incValue
+            if (newCount >= 0 && newCount <= this.mineCount) {
+                this.availableFlagsCount = newCount
+                const elem = document.getElementById('flags-left-counter')
+                elem.innerText = this.availableFlagsCount
+                return true
             }
+            return false
+        },
 
-        })(this);
-
-        const gameField = document.getElementById("game-field");
-        gameField.addEventListener('click', leftClickHandler, {signal: this.unregisterController.signal})
-    },
-
-    updateGameField: function(field) {
-        const game = this
-        const countNeighborMines = function(field) {
-            let surrounding_mines = 0
-            for (let n of game.neighbors(field)) {
-                if (n.classList.contains('mine')) surrounding_mines += 1
+        isWon: function() {
+            const openedFields = document.querySelectorAll('div.field.open')
+            if (this.rows * this.cols - openedFields.length === this.mineCount) {
+                return true
             }
-            field.classList.add('open')  // when we go recursive we need to open too
-            if (surrounding_mines > 0) {
-                field.innerText = surrounding_mines
-                field.classList.add(game.mines2colorClass[surrounding_mines])
-            } else {
-                for (let n of game.neighbors(field)) {
-                    if (!n.classList.contains('open') && !n.classList.contains('mine') && !n.classList.contains('flagged')) {
-                        countNeighborMines(n)
+        },
+
+        updateTime: function() {
+            const now = Math.floor(Date.now() / 1000)
+            const seconds = now - this.timeStart
+            const elem = document.getElementById('elapsed-time-counter')
+            elem.innerText = seconds
+            return seconds
+        },
+
+        // reference solution for "Create mine flagging feature" user story
+        initRightClick: function () {
+            // avoid the usage of global game variable from below
+            const rightClickHandler = (function(game) {
+                return function(e) {
+                    if (game.isWon()) {
+                        game.unregisterController.abort()
+                        return
+                    }
+                    if (!e.target.classList.contains('field')) return;
+                    e.preventDefault();
+                    if (!game.timeStart) game.timeStart = Math.floor(Date.now() / 1000);
+
+                    // if we already have flag - then we clear it and increase the available flags
+                    const inc = e.target.classList.contains('flagged') ? 1 : -1
+                    if (game.updateFlags(inc)) {
+                        e.target.classList.toggle('flagged');
                     }
                 }
+            })(this);
+
+            // let's put this high up on the whole field and let it bubble up from the specific game tile
+            const gameField = document.getElementById("game-field");
+            gameField.addEventListener('contextmenu', rightClickHandler)
+        },
+
+        initLeftClick: function () {
+            const leftClickHandler = (function(game) {
+                return function (e) {
+                    if (!e.target.classList.contains('field')) return;
+                    e.preventDefault()
+                    if (!game.timeStart) game.timeStart = Math.floor(Date.now() / 1000);
+
+                    const field = e.target
+                    const already = field.classList.contains('open') || field.classList.contains('flagged')
+                    if (already) return
+
+                    field.classList.add('open')
+
+                    if (field.classList.contains('mine')) {
+                        const seconds = game.updateTime()
+                        window.alert(`💣  \n(in ${seconds} sec)`)
+
+                        const mineFields = document.querySelectorAll('div.field.mine')
+                        mineFields.forEach((f) => f.classList.add('open'))
+
+                        game.unregisterController.abort()
+                        return
+                        // history.back()
+                    }
+
+                    game.updateGameField(field)
+
+                    if (game.isWon()) {
+                        const seconds = game.updateTime()
+                        window.alert(`🎉 \n(in ${seconds} sec)`)
+
+                        const mineFields = document.querySelectorAll('div.field.mine')
+                        mineFields.forEach((f) => f.classList.add('flagged'))
+                        game.updateFlags(-1)  // should be only one mine left to flag
+
+                        game.unregisterController.abort()
+                        return
+                    }
+                }
+
+            })(this);
+
+            const gameField = document.getElementById("game-field");
+            gameField.addEventListener('click', leftClickHandler, {signal: this.unregisterController.signal})
+        },
+
+        updateGameField: function(field) {
+            const game = this
+            const countNeighborMines = function(field) {
+                let surrounding_mines = 0
+                for (let n of game.neighbors(field)) {
+                    if (n.classList.contains('mine')) surrounding_mines += 1
+                }
+                field.classList.add('open')  // when we go recursive we need to open too
+                if (surrounding_mines > 0) {
+                    field.innerText = surrounding_mines
+                    field.classList.add(game.mines2colorClass[surrounding_mines])
+                } else {
+                    for (let n of game.neighbors(field)) {
+                        if (!n.classList.contains('open') && !n.classList.contains('mine') && !n.classList.contains('flagged')) {
+                            countNeighborMines(n)
+                        }
+                    }
+                }
+                return surrounding_mines
             }
-            return surrounding_mines
-        }
 
-        countNeighborMines(field)
-    },
+            countNeighborMines(field)
+        },
 
-    neighbors: function*(field) {
-        const [row, col] = [+field.dataset.row, +field.dataset.col]
-        let surrounding_coordinates = [
-            [row - 1, col - 1],
-            [row - 1, col],
-            [row - 1, col + 1],
-            [row, col + 1],
-            [row + 1, col + 1],
-            [row + 1, col],
-            [row + 1, col - 1],
-            [row, col - 1],
-        ]
-        for (const coords of surrounding_coordinates) {
-            if (coords[0] < 0 || coords[1] < 0) continue;
-            if (coords[0] >= this.rows || coords[1] >= this.cols) continue;
-            let neighbor = document.querySelector(`.game-field div[data-row="${coords[0]}"][data-col="${coords[1]}"]`)
-            yield neighbor
-        }
-    },
-};
+        neighbors: function*(field) {
+            const [row, col] = [+field.dataset.row, +field.dataset.col]
+            let surrounding_coordinates = [
+                [row - 1, col - 1],
+                [row - 1, col],
+                [row - 1, col + 1],
+                [row, col + 1],
+                [row + 1, col + 1],
+                [row + 1, col],
+                [row + 1, col - 1],
+                [row, col - 1],
+            ]
+            for (const coords of surrounding_coordinates) {
+                if (coords[0] < 0 || coords[1] < 0) continue;
+                if (coords[0] >= this.rows || coords[1] >= this.cols) continue;
+                let neighbor = document.querySelector(`.game-field div[data-row="${coords[0]}"][data-col="${coords[1]}"]`)
+                yield neighbor
+            }
+        },
+    };
 
-game.init();
+    const startButton = document.getElementById('start-game')
+    startButton.addEventListener('click', (e) => {
+        e.preventDefault()
+        let prevSibling = e.currentTarget.previousElementSibling
+        const mines = prevSibling.querySelector('input').value
+        prevSibling = prevSibling.previousElementSibling
+        const size = prevSibling.querySelector('input').value
+
+        window.location.hash = `?size=${size}&mines=${mines}`
+        game.init();
+    })
+}
